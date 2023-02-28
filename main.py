b0c71b02b9681d063b1cc03f4e08d25426455b2f
jae yeong <chujy1224@gmail.com>
2023-02-28 15:44:16 +0900

    커밋1

---
+++ main.py
@@ -0,0 +1,18 @@
+from add import add_func
+from add import sub_func
+
+## 메서드 선언부
+
+
+## 전역 변수 (인스턴트 변수, 클래스 변수)
+num1, num2, result = 100, 200, 0
+
+
+## 메인 코드부 (main() 메서드)
+
+result = add_func(num1, num2)
+print(result)
+
+result = sub_func(num1, num2)
+print(result)
+
